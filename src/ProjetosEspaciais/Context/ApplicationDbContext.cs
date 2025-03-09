d205ce876a44c5bedfe0f98aff6b839ebc46fe08
Hisllaylla Kézia <hisllayllak@gmail.com>
2025-03-09 01:20:11 -0300

    [refactor]: referenciando corretamento as propriedades

--- src/ProjetosEspaciais/Context/ApplicationDbContext.cs
+++ src/ProjetosEspaciais/Context/ApplicationDbContext.cs
@@ -1,7 +1,11 @@
+using ProjetosEspaciais.Mappings;
+
 namespace ProjetosEspaciais.Context;
 
 public class ApplicationDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
         : base(options)
     {
@@ -9,7 +13,6 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Contato> Contatos { get; set; } = null!;
-    public DbSet<Experiencia> Experiencias { get; set; } = null!;
     public DbSet<Licenca> Licencas { get; set; } = null!;
     public DbSet<Localidade> Localidades { get; set; } = null!;
     public DbSet<Membro> Membros { get; set; } = null!;
@@ -17,7 +20,6 @@ public class ApplicationDbContext : DbContext
     public DbSet<Veiculo> Veiculos { get; set; } = null!;
     public DbSet<Equipe> Equipes { get; set; } = null!;
     public DbSet<Documento> Documentos { get; set; } = null!;
-    public DbSet<HistoricoAlteracao> HistoricoAlteracoes { get; set; } = null!;
     public DbSet<Teste> Testes { get; set; } = null!;
     public DbSet<Missao> Missoes { get; set; } = null!;
     public DbSet<Projeto> Projetos { get; set; } = null!;
@@ -28,7 +30,6 @@ public class ApplicationDbContext : DbContext
 
         // mapeamentos
         modelBuilder.ApplyConfiguration(new ContatoMap());
-        modelBuilder.ApplyConfiguration(new ExperienciaMap());
         modelBuilder.ApplyConfiguration(new LicencaMap());
         modelBuilder.ApplyConfiguration(new LocalidadeMap());
         modelBuilder.ApplyConfiguration(new MembroMap());
@@ -36,14 +37,12 @@ public class ApplicationDbContext : DbContext
         modelBuilder.ApplyConfiguration(new VeiculoMap());
         modelBuilder.ApplyConfiguration(new EquipeMap());
         modelBuilder.ApplyConfiguration(new DocumentoMap());
-        modelBuilder.ApplyConfiguration(new HistoricoAlteracaoMap());
         modelBuilder.ApplyConfiguration(new TesteMap());
         modelBuilder.ApplyConfiguration(new MissaoMap());
         modelBuilder.ApplyConfiguration(new ProjetoMap());
 
         // Filtro automático para só trazer registros ativos
         modelBuilder.Entity<Contato>().HasQueryFilter(x => x.Ativo);
-        modelBuilder.Entity<Experiencia>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Licenca>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Localidade>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Membro>().HasQueryFilter(x => x.Ativo);
@@ -51,7 +50,6 @@ public class ApplicationDbContext : DbContext
         modelBuilder.Entity<Veiculo>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Equipe>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Documento>().HasQueryFilter(x => x.Ativo);
-        modelBuilder.Entity<HistoricoAlteracao>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Teste>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Missao>().HasQueryFilter(x => x.Ativo);
         modelBuilder.Entity<Projeto>().HasQueryFilter(x => x.Ativo);
@@ -67,7 +65,7 @@ public class ApplicationDbContext : DbContext
         }
     }
 
-    public new async Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default)
+    public new async Task<bool> SaveChangesAsync(CancellationToken cancellationToken)
     {
         return await base.SaveChangesAsync(cancellationToken) > 0;
     }
